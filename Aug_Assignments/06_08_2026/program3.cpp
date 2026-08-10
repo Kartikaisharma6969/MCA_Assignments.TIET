890bd81759a7eb7e4602788a0a893c12bca9539d
Kartikai <yeskartike4026w@gmail.com>
2026-08-10 21:25:17 +0530

    Created a file for program 1D array K positions left rotation

--- Aug_Assignments/06_08_2026/program3.cpp
+++ Aug_Assignments/06_08_2026/program3.cpp
@@ -1,1 +1,12 @@
 /*Design the logic of a program to rotate a 1-D array by K positions. e.g., for A = {1,2,3,4,5} and K = 2, the left rotation is {3,4,5,1,2} and the right rotation is {4,5,1,2,3}. */
+#include<iostream>
+using namespace std;
+int main(){
+    int A[5] = {1,2,3,4,5};
+    int k ;
+    cout<<"Enter any postion K value where you want to left reverse(Basically index): ";
+    cin>>k;
+ 
+ 
+ 
+}

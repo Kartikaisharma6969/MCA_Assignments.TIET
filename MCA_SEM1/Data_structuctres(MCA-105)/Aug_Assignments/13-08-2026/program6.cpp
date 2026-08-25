af11569e08d38a78b25515ae3a73fa9e65631eeb
Kartikai <yeskartike4026w@gmail.com>
2026-08-25 10:57:10 +0530

    sparse matrix done

--- MCA_SEM1/Data_structuctres(MCA-105)/Aug_Assignments/13-08-2026/program6.cpp
+++ MCA_SEM1/Data_structuctres(MCA-105)/Aug_Assignments/13-08-2026/program6.cpp
@@ -18,6 +18,7 @@ assuming the matrix is represented using a triplet.
  }
  int main(){
    int arr[4][4];
+   int nonzeroelementarr[3][4];
 
 
     for(int i = 0 ; i<4; i++){
@@ -29,10 +30,17 @@ assuming the matrix is represented using a triplet.
     }
 
     cout<<"Your entred array: \n";
+    int count = 0;
 
     for(int i = 0 ; i<4 ; i++){
         for(int  j = 0 ; j<4 ; j++){
+            if(arr[i][j]!=0){
+            nonzeroelementarr[count][0] = i;
+            nonzeroelementarr[count][1] = j;
+            nonzeroelementarr[count][2] = arr[i][j];
+            }
             cout<<arr[i][j]<<" ";
+            count++;
         }
         cout<<"\n";
     }
@@ -41,6 +49,13 @@ assuming the matrix is represented using a triplet.
     if(sparsecount!=0){
         cout<<"The number of non zero elements are: "<<sparsecount;
     }
-  
+
+                for(int k = 0 ; k<4 ; k++){
+                    for(int h = 0 ; h<4 ; h++){
+                        cout<<nonzeroelementarr[k][h]<<" ";
+                    }
+                    cout<<endl;
+                }
+ 
   
  }
